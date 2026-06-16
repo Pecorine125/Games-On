d97166866e0e23844909f767b8f0777a8674d7c6
Pecorine <harahellima@gmail.com>
2026-06-15 22:02:45 -0300

    asd

--- jogos.js
+++ jogos.js
@@ -4,7 +4,7 @@ const meusJogos = [
     { id: 2, nome: "Game 2", capa: "https://img.itch.zone/aW1nLzI3NjUzODc3LnBuZw==/315x250%23c/lCJ7s1.png", url: "https://html-classic.itch.zone/html/17791990/MonsterTutor-0.2-web/index.html?v=1780499465" },
     { id: 3, nome: "Game 3", capa: "https://img.itch.zone/aW1nLzI0OTU0OTg3LmpwZw==/315x250%23c/pDZXFd.jpg", url: "https://html-classic.itch.zone/html/17694679/index.html?v=1779820873" },
     { id: 4, nome: "Game 4", capa: "https://img.itch.zone/aW1nLzIzMTI2MzUxLmdpZg==/315x250%23c/7TYtQT.gif", url: "https://html.itch.zone/html/14112196/index.html" },
-    { id: 5, nome: "Game 5", capa: "", url: "" },
+    { id: 5, nome: "Game 5", capa: "https://img.itch.zone/aW1nLzI2MzY0NTMyLmpwZw==/315x250%23c/X%2Bkr5D.jpg", url: "https://html-classic.itch.zone/html/16956657/index.html?v=1774660687" },
     { id: 6, nome: "Game 6", capa: "", url: "" },
     { id: 7, nome: "Game 7", capa: "", url: "" },
     { id: 8, nome: "Game 8", capa: "", url: "" },
