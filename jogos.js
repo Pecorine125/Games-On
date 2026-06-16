77c1a40eb67ec2efa9934bcb7590f2db6ef3c842
Pecorine <harahellima@gmail.com>
2026-06-15 21:57:02 -0300

    asd

--- jogos.js
+++ jogos.js
@@ -1,35 +1,10 @@
 // Banco de dados dos seus jogos. Já configurado com o proxy para pular o bloqueio de imagens.
 const meusJogos = [
-    {
-        id: 1,
-        nome: "Game 1",
-        capa: "https://images.weserv.nl/?url=fgn.cdn.serverable.com/common/flashes/w/b/becoming_a_king.webp",
-        url: "https://fgn.cdn.serverable.com/common/flashes/nf/becoming_a_king/index.html?d=20250404182532"
-    },
-    {
-        id: 2,
-        nome: "Game 2",
-        capa: "https://images.weserv.nl/?url=fgn.cdn.serverable.com/common/flashes/w/b/biological_lust.webp",
-        url: "https://fgn.cdn.serverable.com/common/flashes/nf/biological_lust/index.html?d=20250530005319"
-    },
-    {
-        id: 3,
-        nome: "Game 3",
-        capa: "https://images.weserv.nl/?url=fgn.cdn.serverable.com/common/flashes/w/o/orange_smash.webp",
-        url: "https://orange_smash_v_0_9_1.fgn.cdn.serverable.com/common/flashes/nf/orange_smash/v_0.9.1/index.html?d=20260427011752"
-    },
-    {
-        id: 4,
-        nome: "Game 4",
-        capa: "https://images.weserv.nl/?url=fgn.cdn.serverable.com/common/flashes/w/m/my_hardcore_harem.webp",
-        url: "https://my_hardcore_harem.fgn.cdn.serverable.com/common/flashes/nf/my_hardcore_harem/index.html?d=20251103002121"
-    },
-    {
-        id: 5,
-        nome: "Game 5",
-        capa: "https://images.weserv.nl/?url=fgn.cdn.serverable.com/common/flashes/w/m/meltwish_valley.webp",
-        url: "https://meltwish_valley_v_0_3_1.fgn.cdn.serverable.com/common/flashes/nf/meltwish_valley/v_0.3.1/index.html?d=20260221011508"
-    },
+    { id: 1, nome: "Game 1", capa: "", url: "" },
+    { id: 2, nome: "Game 2", capa: "", url: "" },
+    { id: 3, nome: "Game 3", capa: "", url: "" },
+    { id: 4, nome: "Game 4", capa: "", url: "" },
+    { id: 5, nome: "Game 5", capa: "", url: "" },
     { id: 6, nome: "Game 6", capa: "", url: "" },
     { id: 7, nome: "Game 7", capa: "", url: "" },
     { id: 8, nome: "Game 8", capa: "", url: "" },
