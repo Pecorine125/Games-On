d3bce9c09fa3242b98bc6416931c87f34aaed32f
Pecorine <harahellima@gmail.com>
2026-06-15 21:47:15 -0300

    asd

--- jogos.js
+++ jogos.js
@@ -3,75 +3,40 @@ const meusJogos = [
     {
         id: 1,
         nome: "Game 1",
-        capa: "https://fgn.cdn.serverable.com/common/flashes/w/b/becoming_a_king.webp",
+        capa: "https://images.weserv.nl/?url=fgn.cdn.serverable.com/common/flashes/w/b/becoming_a_king.webp",
         url: "https://fgn.cdn.serverable.com/common/flashes/nf/becoming_a_king/index.html?d=20250404182532"
     },
     {
         id: 2,
         nome: "Game 2",
-        capa: "https://fgn.cdn.serverable.com/common/flashes/w/b/biological_lust.webp",
+        capa: "https://images.weserv.nl/?url=fgn.cdn.serverable.com/common/flashes/w/b/biological_lust.webp",
         url: "https://fgn.cdn.serverable.com/common/flashes/nf/biological_lust/index.html?d=20250530005319"
     },
     {
         id: 3,
         nome: "Game 3",
-        capa: "https://fgn.cdn.serverable.com/common/flashes/w/o/orange_smash.webp",
+        capa: "https://images.weserv.nl/?url=fgn.cdn.serverable.com/common/flashes/w/o/orange_smash.webp",
         url: "https://orange_smash_v_0_9_1.fgn.cdn.serverable.com/common/flashes/nf/orange_smash/v_0.9.1/index.html?d=20260427011752"
     },
     {
         id: 4,
         nome: "Game 4",
-        capa: "https://fgn.cdn.serverable.com/common/flashes/w/m/my_hardcore_harem.webp",
+        capa: "https://images.weserv.nl/?url=fgn.cdn.serverable.com/common/flashes/w/m/my_hardcore_harem.webp",
         url: "https://my_hardcore_harem.fgn.cdn.serverable.com/common/flashes/nf/my_hardcore_harem/index.html?d=20251103002121"
     },
     {
         id: 5,
         nome: "Game 5",
-        capa: "https://fgn.cdn.serverable.com/common/flashes/w/m/meltwish_valley.webp",
+        capa: "https://images.weserv.nl/?url=fgn.cdn.serverable.com/common/flashes/w/m/meltwish_valley.webp",
         url: "https://meltwish_valley_v_0_3_1.fgn.cdn.serverable.com/common/flashes/nf/meltwish_valley/v_0.3.1/index.html?d=20260221011508"
     },
-    {
-        id: 6,
-        nome: "Game 6",
-        capa: "",
-        url: ""
-    },
-    {
-        id: 7,
-        nome: "Game 7",
-        capa: "",
-        url: ""
-    },
-    {
-        id: 8,
-        nome: "Game 8",
-        capa: "",
-        url: ""
-    },
-    {
-        id: 9,
-        nome: "Game 9",
-        capa: "",
-        url: ""
-    },
-    {
-        id: 10,
-        nome: "Game 10",
-        capa: "",
-        url: ""
-    },
-    {
-        id: 11,
-        nome: "Game 11",
-        capa: "",
-        url: ""
-    },
-    {
-        id: 12,
-        nome: "Game 12",
-        capa: "",
-        url: ""
-    },
+    { id: 6, nome: "Game 6", capa: "", url: "" },
+    { id: 7, nome: "Game 7", capa: "", url: "" },
+    { id: 8, nome: "Game 8", capa: "", url: "" },
+    { id: 9, nome: "Game 9", capa: "", url: "" },
+    { id: 10, nome: "Game 10", capa: "", url: "" },
+    { id: 11, nome: "Game 11", capa: "", url: "" },
+    { id: 12, nome: "Game 12", capa: "", url: "" }
 ];
 
 // Exporta a lista para que o arquivo app.js consiga ler
