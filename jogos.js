17ca4c9183520582c88c667cf3d4f07dc63e05cc
Pecorine <harahellima@gmail.com>
2026-06-15 22:01:46 -0300

    asd

--- jogos.js
+++ jogos.js
@@ -1,9 +1,9 @@
 // Banco de dados dos seus jogos. Já configurado com o proxy para pular o bloqueio de imagens.
 const meusJogos = [
     { id: 1, nome: "Game 1", capa: "https://img.itch.zone/aW1nLzI3MTUyMzQ1LmdpZg==/315x250%23c/6xHqNm.gif", url: "https://worldtamergame.com/release/index.html" },
-    { id: 2, nome: "Game 2", capa: "", url: "" },
-    { id: 3, nome: "Game 3", capa: "", url: "" },
-    { id: 4, nome: "Game 4", capa: "", url: "" },
+    { id: 2, nome: "Game 2", capa: "https://img.itch.zone/aW1nLzI3NjUzODc3LnBuZw==/315x250%23c/lCJ7s1.png", url: "https://html-classic.itch.zone/html/17791990/MonsterTutor-0.2-web/index.html?v=1780499465" },
+    { id: 3, nome: "Game 3", capa: "https://img.itch.zone/aW1nLzI0OTU0OTg3LmpwZw==/315x250%23c/pDZXFd.jpg", url: "https://html-classic.itch.zone/html/17694679/index.html?v=1779820873" },
+    { id: 4, nome: "Game 4", capa: "https://img.itch.zone/aW1nLzIzMTI2MzUxLmdpZg==/315x250%23c/7TYtQT.gif", url: "https://html.itch.zone/html/14112196/index.html" },
     { id: 5, nome: "Game 5", capa: "", url: "" },
     { id: 6, nome: "Game 6", capa: "", url: "" },
     { id: 7, nome: "Game 7", capa: "", url: "" },
