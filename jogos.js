cd78f05100d2d207334c35b4c936e62f66a70a43
Pecorine <harahellima@gmail.com>
2026-06-15 21:36:55 -0300

    asd

--- jogos.js
+++ jogos.js
@@ -11,7 +11,61 @@ const meusJogos = [
         nome: "Game 2",
         capa: "https://fgn.cdn.serverable.com/common/flashes/w/b/biological_lust.webp",
         url: "https://fgn.cdn.serverable.com/common/flashes/nf/biological_lust/index.html?d=20250530005319"
-    }
+    },
+    {
+        id: 4,
+        nome: "Game 3",
+        capa: "",
+        url: ""
+    },
+    {
+        id: 5,
+        nome: "Game 5",
+        capa: "",
+        url: ""
+    },
+    {
+        id: 6,
+        nome: "Game 6",
+        capa: "",
+        url: ""
+    },
+    {
+        id: 7,
+        nome: "Game 7",
+        capa: "",
+        url: ""
+    },
+    {
+        id: 8,
+        nome: "Game 8",
+        capa: "",
+        url: ""
+    },
+    {
+        id: 9,
+        nome: "Game 9",
+        capa: "",
+        url: ""
+    },
+    {
+        id: 10,
+        nome: "Game 10",
+        capa: "",
+        url: ""
+    },
+    {
+        id: 11,
+        nome: "Game 11",
+        capa: "",
+        url: ""
+    },
+    {
+        id: 12,
+        nome: "Game 12",
+        capa: "",
+        url: ""
+    },
 ];
 
 // Exporta a lista para que o arquivo app.js consiga ler
