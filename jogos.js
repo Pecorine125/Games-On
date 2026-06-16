f2b85de4bfdcdc3e659d6c5ab45eae9ee41e93db
Pecorine <harahellima@gmail.com>
2026-06-15 21:57:47 -0300

    asd

--- jogos.js
+++ jogos.js
@@ -1,6 +1,6 @@
 // Banco de dados dos seus jogos. Já configurado com o proxy para pular o bloqueio de imagens.
 const meusJogos = [
-    { id: 1, nome: "Game 1", capa: "", url: "" },
+    { id: 1, nome: "Game 1", capa: "https://img.itch.zone/aW1nLzI3MTUyMzQ1LmdpZg==/315x250%23c/6xHqNm.gif", url: "https://worldtamergame.com/release/index.html" },
     { id: 2, nome: "Game 2", capa: "", url: "" },
     { id: 3, nome: "Game 3", capa: "", url: "" },
     { id: 4, nome: "Game 4", capa: "", url: "" },
