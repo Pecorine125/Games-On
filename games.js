b52e5e604e16a425f6bdc9ecd95556db3a2a56f8
Pecorine <harahellima@gmail.com>
2026-06-18 10:17:21 -0300

    asd

---
+++ games.js
@@ -0,0 +1,25 @@
+// games.js
+const games = [
+    {
+        id: "game8",
+        nome: "Game 8 (Horny Doctor)",
+        pasta: "Game 8",
+        capa: "https://img.itch.zone/aW1nLzIwNjU2NTY1LmpwZw==/315x250%23c/zqHoJG.jpg",
+        temWeb: true                      // mude para false se não tiver versão web
+    },
+    {
+        id: "game7",
+        nome: "Game 7",
+        pasta: "Game 7",
+        capa: "https://img.itch.zone/aW1nLzIwNjcyMTc0LmpwZw==/315x250%23c/ua5aDt.jpg",
+        temWeb: true
+    }
+    // Adicione novos jogos aqui ↓
+    // {
+    //     id: "game9",
+    //     nome: "Nome do Jogo",
+    //     pasta: "Nome da Pasta",
+    //     capa: "link-da-imagem.jpg",
+    //     temWeb: true
+    // }
+];
