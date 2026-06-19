73efc4c9f1c5919eb69905eef2b72dd4ebb41d60
Pecorine <harahellima@gmail.com>
2026-06-18 22:39:05 -0300

    asd

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 async function carregarJogos() {
     const grid = document.getElementById('grid-jogos');
     try {
-        const resposta = await fetch('jogos.json');
-        const jogos = await resposta.json();
-        grid.innerHTML = jogos.map(jogo => `
-            <div class="card" onclick="abrirJogo('${jogo.url}')">
-                <img src="${jogo.capa}" alt="${jogo.nome}">
-                <p>${jogo.nome}</p>
+        const res = await fetch('jogos.json');
+        const jogos = await res.json();
+        grid.innerHTML = jogos.map(j => `
+            <div class="card" onclick="abrirJogo('${j.url}')">
+                <img src="${j.capa}" alt="${j.nome}">
+                <p>${j.nome}</p>
             </div>
         `).join('');
-    } catch (err) {
-        grid.innerHTML = "<p>Erro ao carregar jogos. Verifique o arquivo JSON.</p>";
-    }
+    } catch (e) { console.error(e); }
 }
 
 function abrirJogo(url) {
-    const wrapper = document.getElementById('iframe-wrapper');
-    wrapper.innerHTML = `<iframe src="${url}" allow="autoplay; fullscreen" allowfullscreen></iframe>`;
+    document.getElementById('iframe-wrapper').innerHTML = 
+        `<iframe src="${url}" allow="autoplay; fullscreen" allowfullscreen></iframe>`;
     document.getElementById('container-jogo').classList.remove('hidden');
 }
 
@@ -24,5 +22,4 @@ function fecharJogo() {
     document.getElementById('iframe-wrapper').innerHTML = '';
     document.getElementById('container-jogo').classList.add('hidden');
 }
-
 carregarJogos();
