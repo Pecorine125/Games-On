d9e99fb77db94b34d18227ca2fec4b9d9034bb1a
Pecorine <harahellima@gmail.com>
2026-07-20 14:35:47 -0300

    asd

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ const gamesData = [
     { title: "Breaking The Lust", image: "https://cdn77.gamesofdesire.com/files/img/breaking-the-lust-2.webp", url: "https://cdn77.gamesofdesire.com/files/html5/breaking-the-lust/index.html" },
     { title: "Paprika Trainer", image: "https://cdn77.gamesofdesire.com/files/img/paprika-trainer-2.webp", url: "https://cdn77.gamesofdesire.com/files/html5_new/paprika-trainer-1-2-0-0/index.html" },
     { title: "Milfy Cummy: Gloria Returns", image: "https://cdn77.gamesofdesire.com/files/img/milfy-cummy-gloria-returns-2.webp", url: "https://cdn77.gamesofdesire.com/files/html5/milfy-cummy-gloria-returns/index.html" },
-    { title: "", image: "", url: "" },
+    { title: "Nano-control", image: "https://cdn77.gamesofdesire.com/files/img/nano-control-2.webp", url: "https://cdn77.gamesofdesire.com/files/html5/nano-control-v1-0c/index.html" },
     { title: "", image: "", url: "" },
     { title: "", image: "", url: "" },
     { title: "", image: "", url: "" },
